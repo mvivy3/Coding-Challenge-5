4ad845b81baa0489918d74f53ab614295281b7d6
mvivy3 <meredithyen@usf.edu>
2025-02-05 22:07:20 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -8,3 +8,20 @@ let customer = {
 console.log(customer);
 console.log(`Customer profile: Name - ${customer.name}, Age - ${customer.age}, Email - ${customer.email}`);
 
+
+// Task 2: Order Details
+
+// Declare an object order with properties
+let order = {
+    orderId: 56789,
+    totalAmount: 250,
+    status: "Processing"
+};
+// Add a method "displayOrder" to log the order details using "this"
+order.displayOrder = function() {
+    console.log("Order Id: " + this.orderId, "Total Amount: " + this.totalAmount, "Status: " + this.status);
+};
+// Call the method 
+order.displayOrder();
+// Log the order details
+console.log(order);
